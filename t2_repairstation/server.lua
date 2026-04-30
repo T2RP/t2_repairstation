01643da165e975c1cc2f140a28211ef1a91e158f
Ƶ𝔦x <158811830+zixja@users.noreply.github.com>
2026-04-30 04:40:51 -0400

    Update payAndRepair function to use Config.RepairPrice

--- t2_repairstation/server.lua
+++ t2_repairstation/server.lua
@@ -1,8 +1,8 @@
-lib.callback.register('qbx_repairstation:payAndRepair', function(source, option)
+lib.callback.register('qbx_repairstation:payAndRepair', function(source)
     local player = exports.qbx_core:GetPlayer(source)
     if not player then return false, 'Player not found' end
 
-    if player.Functions.RemoveMoney('cash', option.price, 'repair_station') then
+    if player.Functions.RemoveMoney('cash', Config.RepairPrice, 'repair_station') then
         return true
     end
 
